d1a003c205113447b65e955988d0466fd253b8b3
sorrick <63976854+sorrick@users.noreply.github.com>
2021-04-07 20:37:07 -0500

    Initial commit

---
+++ FlappyHunt/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace FlappyHunt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.picHunter = new System.Windows.Forms.PictureBox();
+            this.picDog = new System.Windows.Forms.PictureBox();
+            this.picHive = new System.Windows.Forms.PictureBox();
+            this.picGround = new System.Windows.Forms.PictureBox();
+            this.huntTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picHunter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGround)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Elephant", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.Location = new System.Drawing.Point(364, 777);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(159, 41);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // picHunter
+            // 
+            this.picHunter.Image = global::FlappyHunt.Properties.Resources.hunterBlue_removebg_preview;
+            this.picHunter.Location = new System.Drawing.Point(226, 319);
+            this.picHunter.Name = "picHunter";
+            this.picHunter.Size = new System.Drawing.Size(150, 82);
+            this.picHunter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHunter.TabIndex = 1;
+            this.picHunter.TabStop = false;
+            // 
+            // picDog
+            // 
+            this.picDog.Image = global::FlappyHunt.Properties.Resources.hellhound_removebg_preview;
+            this.picDog.Location = new System.Drawing.Point(693, -38);
+            this.picDog.Name = "picDog";
+            this.picDog.Size = new System.Drawing.Size(150, 252);
+            this.picDog.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDog.TabIndex = 2;
+            this.picDog.TabStop = false;
+            this.picDog.Click += new System.EventHandler(this.picDog_Click);
+            // 
+            // picHive
+            // 
+            this.picHive.Image = global::FlappyHunt.Properties.Resources.Hive_removebg_preview;
+            this.picHive.Location = new System.Drawing.Point(546, 516);
+            this.picHive.Name = "picHive";
+            this.picHive.Size = new System.Drawing.Size(150, 267);
+            this.picHive.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picHive.TabIndex = 3;
+            this.picHive.TabStop = false;
+            this.picHive.Click += new System.EventHandler(this.picHive_Click);
+            // 
+            // picGround
+            // 
+            this.picGround.Image = global::FlappyHunt.Properties.Resources.ground;
+            this.picGround.Location = new System.Drawing.Point(-18, 757);
+            this.picGround.Name = "picGround";
+            this.picGround.Size = new System.Drawing.Size(1043, 75);
+            this.picGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGround.TabIndex = 4;
+            this.picGround.TabStop = false;
+            // 
+            // huntTimer
+            // 
+            this.huntTimer.Enabled = true;
+            this.huntTimer.Interval = 20;
+            this.huntTimer.Tick += new System.EventHandler(this.huntTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(986, 844);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.picGround);
+            this.Controls.Add(this.picHive);
+            this.Controls.Add(this.picDog);
+            this.Controls.Add(this.picHunter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picHunter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGround)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox picHunter;
+        private System.Windows.Forms.PictureBox picDog;
+        private System.Windows.Forms.PictureBox picHive;
+        private System.Windows.Forms.PictureBox picGround;
+        private System.Windows.Forms.Timer huntTimer;
+    }
+}
+
---
+++ FlappyHunt/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyHunt
+{
+    public partial class Form1 : Form
+    {
+        //Need to keep track of some info for the game
+        int pipeSpeed = 5; //How fast the pipe will move
+        int gravity = 10; //Speed of gravity
+        int score = 0; //Keep track of the score/Distance traveled
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void gameKeyDown(object sender, KeyEventArgs e)
+        {
+            //This will basically control the movement of pushing the space bar
+            if (e.KeyCode == Keys.Space)
+            {
+                //It will appear like the hunter is going up
+                gravity = -15;
+            }
+
+        }
+
+        private void gameKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                //Once the space bar is released the gravity is set again so hunter will fall
+                gravity = 5;
+            }
+
+        }
+
+        private void endHunt()
+        {
+            //The hunt will end once this function runs
+            huntTimer.Stop(); //Stop the timer
+            lblScore.Text = "Your Hunt is over!!!"; //Displays the endgame message, += appends instead of overriding 
+        }
+        private void huntTimerEvent(object sender, EventArgs e)
+        {
+            picHunter.Top += gravity; //Adds the speed to the gravity so the hunter will fall
+            picHive.Left -= pipeSpeed; //Moves the hive towards the left
+            picDog.Left -= pipeSpeed; //Moves the dogs towards the left
+            lblScore.Text = "Score: " + score; //Updates the score label
+
+            //Check if the hive leaves the screen
+            if (picHive.Left < -150)
+            {
+                //If the hive left the screen then the position is reset and score increased
+                picHive.Left = 800;
+                score++;
+            }
+            //Check if the dog leaves the screen
+            if (picDog.Left < -180)
+            {
+                //If the dog left the screen then the position is reset and score increased
+                picDog.Left = 950;
+                
+            }
+
+            //Need to check if the hunter hits the ground or one of the obstacles
+            if (picHunter.Bounds.IntersectsWith(picDog.Bounds) || picHunter.Bounds.IntersectsWith(picHive.Bounds) ||
+                picHunter.Bounds.IntersectsWith(picGround.Bounds) || picHunter.Top < -25)
+            {
+                //If any condition is met then the game will end
+                endHunt();
+            }
+
+            //If the score increases over 5 then the speed increaes
+            if(score < 5)
+            {
+                pipeSpeed = 15;
+            }
+        
+
+        }
+        private void picHive_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picDog_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
